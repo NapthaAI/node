6d5c0e33a245ec9c259969cffeae5a22d3fbab20
Mohamed Arshath <marshath@gmail.com>
2024-08-02 05:31:43 +0000

    modify db to avoid making multiple connections

--- node/comms/orchestration.py
+++ node/comms/orchestration.py
@@ -8,7 +8,7 @@ logger = get_logger(__name__)
 
 async def create_task_run(module_run_input: ModuleRunInput) -> ModuleRun:
     logger.info(f"Creating module run for worker node {module_run_input.worker_nodes[0]}: {module_run_input}")
-    db = await DB()
+    db = DB()
     module_run = await db.create_module_run(module_run_input)
     logger.info(f"Created module run for worker node {module_run_input.worker_nodes[0]}: {module_run}")
     return module_run
@@ -16,7 +16,7 @@ async def create_task_run(module_run_input: ModuleRunInput) -> ModuleRun:
 
 async def update_task_run(module_run: ModuleRun) -> ModuleRun:
     logger.info(f"Updating module run for worker node {module_run.worker_nodes[0]}: {module_run}")
-    db = await DB()
+    db = DB()
     updated_module_run = await db.update_module_run(module_run.id, module_run)
     logger.info(f"Updated module run for worker node {module_run.worker_nodes[0]}: {updated_module_run}")
     return updated_module_run
--- node/comms/user.py
+++ node/comms/user.py
@@ -10,7 +10,7 @@ router = APIRouter()
 
 async def register_user(user_input: Dict) -> Dict:
     logger.info(f"Received request to register user.")
-    db = await DB()
+    db = DB()
     user = await db.create_user(user_input)
     logger.info(f"Created user: {user}")
     return user
@@ -20,7 +20,7 @@ async def check_user(user_input: Dict) -> Dict:
     logger.info(f"Received request to check user.")
     user_input['id'] = f"user:{user_input['public_key']}"
 
-    db = await DB()
+    db = DB()
     user = await db.get_user(user_input)
 
     if user is not None:
