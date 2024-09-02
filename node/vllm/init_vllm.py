85d0d003844eb92ab1158438e6a7e8bf7feda6ec
Mohamed Arshath <marshath@gmail.com>
2024-09-02 01:48:28 +0000

    add default envs for dockers

--- node/vllm/init_vllm.py
+++ node/vllm/init_vllm.py
@@ -1,14 +1,30 @@
 import os
 import subprocess
+from node.utils import get_logger
+
+logger = get_logger(__name__)
+
+GPU = os.getenv("GPU", False)
+if GPU == 'false':
+    GPU = False
+elif GPU == 'true':
+    GPU = True
+
+logger.info(f"GPU: {GPU}")
+
 
 def setup_vllm(model):
     current_dir = os.path.dirname(os.path.abspath(__file__))
     chat_template_path = os.path.join(current_dir, "chatml.jinja")
-    
-    command = [
-        "vllm", "serve", model,
-        "--max-model-len", "2500",
-        "--chat-template", chat_template_path
-    ]
-    
+    if GPU:
+        command = [
+            "vllm", "serve", model,
+            "--max-model-len", "2500",
+            "--chat-template", chat_template_path
+        ]
+    else:
+        command = [
+            "vllm", "serve", model,
+            "--chat-template", chat_template_path
+        ]
     subprocess.run(command, check=True)
