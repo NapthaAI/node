d8fd5784d3aa1ccc4636905d2d16f79a6c911ca5
Richard Blythman <richardblythman@gmail.com>
2024-09-05 12:37:31 +0100

    better error logging for orchestrator endpoints

--- node/comms/orchestration.py
+++ node/comms/orchestration.py
@@ -1,23 +1,34 @@
 from naptha_sdk.schemas import ModuleRun, ModuleRunInput
 from node.storage.db.db import DB
 from node.utils import get_logger
+import traceback
 from typing import Dict
 
 logger = get_logger(__name__)
 
 
 async def create_task_run(module_run_input: ModuleRunInput) -> ModuleRun:
-    logger.info(f"Creating module run for worker node {module_run_input.worker_nodes[0]}: {module_run_input}")
-    db = DB()
-    module_run = await db.create_module_run(module_run_input)
-    logger.info(f"Created module run for worker node {module_run_input.worker_nodes[0]}: {module_run}")
-    return module_run
-
+    try:
+        logger.info(f"Creating module run for worker node {module_run_input.worker_nodes[0]}: {module_run_input}")
+        db = DB()
+        module_run = await db.create_module_run(module_run_input)
+        logger.info(f"Created module run for worker node {module_run_input.worker_nodes[0]}: {module_run}")
+        return module_run
+    except Exception as e:
+        logger.error(f"Failed to create module run for worker node {module_run_input.worker_nodes[0]}: {module_run_input}")
+        error_details = traceback.format_exc()
+        logger.error(f"Full traceback: {error_details}")
+        raise Exception(f"Failed to create module run for worker node {module_run_input.worker_nodes[0]}: {module_run_input}")
 
 async def update_task_run(module_run: ModuleRun) -> ModuleRun:
-    logger.info(f"Updating module run for worker node {module_run.worker_nodes[0]}: {module_run}")
-    db = DB()
-    updated_module_run = await db.update_module_run(module_run.id, module_run)
-    logger.info(f"Updated module run for worker node {module_run.worker_nodes[0]}: {updated_module_run}")
-    return updated_module_run
-
+    try:
+        logger.info(f"Updating module run for worker node {module_run.worker_nodes[0]}: {module_run}")
+        db = DB()
+        updated_module_run = await db.update_module_run(module_run.id, module_run)
+        logger.info(f"Updated module run for worker node {module_run.worker_nodes[0]}: {updated_module_run}")
+        return updated_module_run
+    except Exception as e:
+        logger.error(f"Failed to update module run for worker node {module_run.worker_nodes[0]}: {module_run}")
+        error_details = traceback.format_exc()
+        logger.error(f"Full traceback: {error_details}")
+        raise Exception(f"Failed to update module run for worker node {module_run.worker_nodes[0]}: {module_run}")
