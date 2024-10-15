767326378b9a437c64a124400e4a3c7c463b9a29
Mohamed Arshath <marshath@gmail.com>
2024-10-15 10:22:34 +0000

    configs to take env variables

--- node/config.py
+++ node/config.py
@@ -14,45 +14,45 @@ IN_DOCKER=os.getenv("IN_DOCKER", "false").lower() == "true"
 GPU=os.getenv("GPU", "false").lower() == "true"
 NUM_GPUS=0
 VRAM=0
-DOCKER_JOBS=False
+DOCKER_JOBS=os.getenv("DOCKER_JOBS", "false").lower() == "true"
 DEV_MODE=True
 
 # Servers
-NODE_TYPE="direct"
-SERVER_TYPE="http" # http or ws
-NODE_IP="http://localhost"
-NODE_PORT=7001
-NODE_ROUTING="ws://node.naptha.ai:8765"
-NUM_SERVERS=1
+NODE_TYPE=os.getenv("NODE_TYPE", "direct")
+SERVER_TYPE=os.getenv("SERVER_TYPE", "http") # http or ws or grpc
+NODE_IP=os.getenv("NODE_IP", "http://localhost")
+NODE_PORT=int(os.getenv("NODE_PORT", 7001))
+NODE_ROUTING=os.getenv("NODE_ROUTING", "ws://node.naptha.ai:8765")
+NUM_SERVERS=int(os.getenv("NUM_SERVERS", 1))
 
 # MQ
-CELERY_BROKER_URL="amqp://localhost:5672/"
+CELERY_BROKER_URL=os.getenv("CELERY_BROKER_URL", "amqp://localhost:5672/")
 
 # LLMs
-LLM_BACKEND="vllm"
-VLLM_MODEL="NousResearch/Hermes-3-Llama-3.1-8B"
-OLLAMA_MODELS="phi"
+LLM_BACKEND=os.getenv("LLM_BACKEND", "vllm")
+VLLM_MODEL=os.getenv("VLLM_MODEL", "NousResearch/Hermes-3-Llama-3.1-8B")
+OLLAMA_MODELS=os.getenv("OLLAMA_MODELS", "phi")
 
 # Local DB
-SURREALDB_PORT=3002
-DB_NS="naptha"
-DB_DB="naptha"
-DB_URL="ws://localhost:3002/rpc"
+SURREALDB_PORT=int(os.getenv("SURREALDB_PORT", 3002))
+DB_NS=os.getenv("DB_NS", "naptha")
+DB_DB=os.getenv("DB_DB", "naptha")
+DB_URL=os.getenv("DB_URL", "ws://localhost:3002/rpc")
 
 # Storage
 file_path = Path(__file__).resolve()
 repo_dir = file_path.parent.parent
 BASE_OUTPUT_DIR=f"{repo_dir}/node/storage/fs"
 AGENTS_SOURCE_DIR=f"{repo_dir}/node/storage/hub/agents"
-IPFS_GATEWAY_URL="/dns/provider.akash.pro/tcp/31832/http"
+IPFS_GATEWAY_URL=os.getenv("IPFS_GATEWAY_URL", "/dns/provider.akash.pro/tcp/31832/http")
 
 # Hub
-LOCAL_HUB=False
-LOCAL_HUB_URL="ws://localhost:3001/rpc"
-PUBLIC_HUB_URL="ws://node.naptha.ai:3001/rpc"
-HUB_DB_PORT=3001
-HUB_NS="naptha"
-HUB_DB="naptha"
+LOCAL_HUB=os.getenv("LOCAL_HUB", "false").lower() == "true"
+LOCAL_HUB_URL=os.getenv("LOCAL_HUB_URL", "ws://localhost:3001/rpc")
+PUBLIC_HUB_URL=os.getenv("PUBLIC_HUB_URL", "ws://node.naptha.ai:3001/rpc")
+HUB_DB_PORT=int(os.getenv("HUB_DB_PORT", 3001))
+HUB_NS=os.getenv("HUB_NS", "naptha")
+HUB_DB=os.getenv("HUB_DB", "naptha")
 
 def get_node_config():
     """Get the node configuration."""
