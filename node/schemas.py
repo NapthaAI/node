29cc65a278180573da1530b966fe2b5fb09d5f11
Mohamed Arshath <marshath@gmail.com>
2024-11-12 02:51:27 +0000

    change schemas to take personas_urls

--- node/schemas.py
+++ node/schemas.py
@@ -76,6 +76,7 @@ class AgentRun(BaseModel):
     input_schema_ipfs_hash: Optional[str] = None
     agent_source_url: Optional[str] = None
     agent_version: Optional[str] = None
+    personas_urls: Optional[List[str]] = None
 
     class Config:
         allow_mutation = True
@@ -110,6 +111,7 @@ class AgentRunInput(BaseModel):
     parent_runs: List["AgentRun"] = []
     agent_source_url: Optional[str] = None
     agent_version: Optional[str] = None
+    personas_urls: Optional[List[str]] = None
 
     def model_dict(self):
         model_dict = self.dict()
