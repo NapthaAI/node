421cf7532f4a68f2900654c3ac705b28d1b48ee8
Richard Blythman <richardblythman@gmail.com>
2024-11-18 23:06:36 +0000

    update schema defaults

--- node/schemas.py
+++ node/schemas.py
@@ -34,11 +34,11 @@ class LLMClientType(str, Enum):
     OLLAMA = "ollama"
 
 class LLMConfig(BaseModel):
-    config_name: str
-    client: LLMClientType
+    config_name: Optional[str] = "llm_config"
+    client: Optional[LLMClientType] = None
     model: Optional[str] = None
-    max_tokens: int = 400
-    temperature: float = 0
+    max_tokens: Optional[int] = 400
+    temperature: Optional[float] = 0
     api_base: Optional[str] = None
 
 class AgentModuleType(str, Enum):
@@ -59,42 +59,42 @@ class AgentModule(BaseModel):
 
 class AgentConfig(BaseModel):
     config_name: Optional[str] = "agent_config"
-    llm_config: Optional[LLMConfig] = None
+    llm_config: Optional[LLMConfig] = LLMConfig()
     persona_module: Optional[Union[Dict, BaseModel]] = None
     system_prompt: Optional[Union[Dict, BaseModel]] = {'role': 'system', 'content': 'You are a helpful assistant.'}
 
 class OrchestratorConfig(BaseModel):
-    config_name: str
-    max_rounds: str
+    config_name: Optional[str] = "orchestrator_config"
+    max_rounds: Optional[int] = 5
 
 class EnvironmentConfig(BaseModel):
-    config_name: str
-    environment_type: str
+    config_name: Optional[str] = "environment_config"
+    environment_type: Optional[str] = None
 
 class DataGenerationConfig(BaseModel):
-    save_outputs: bool = False
-    save_outputs_location: str = "node"
+    save_outputs: Optional[bool] = False
+    save_outputs_location: Optional[str] = "node"
     save_outputs_path: Optional[str] = None
-    save_inputs: bool = False
-    save_inputs_location: str = "node"
+    save_inputs: Optional[bool] = False
+    save_inputs_location: Optional[str] = "node"
 
 class AgentDeployment(BaseModel):
-    name: str
-    module: Union[Dict, AgentModule]
+    name: Optional[str] = "agent_deployment"
+    module: Optional[Union[Dict, AgentModule]] = None
     worker_node_url: Optional[str] = "http://localhost:7001"
-    agent_config: Optional[AgentConfig] = None
+    agent_config: Optional[AgentConfig] = AgentConfig()
     data_generation_config: Optional[DataGenerationConfig] = DataGenerationConfig()
 
 class OrchestratorDeployment(BaseModel):
-    name: str
-    module: str
+    name: Optional[str] = "orchestrator_deployment"
+    module: Dict
     orchestrator_node_url: Optional[str] = "http://localhost:7001"
-    orchestrator_config: Optional[OrchestratorConfig] = None
+    orchestrator_config: Optional[OrchestratorConfig] = OrchestratorConfig()
 
 class EnvironmentDeployment(BaseModel):
-    name: str
+    name: Optional[str] = "environment_deployment"
     environment_node_url: str
-    environment_config: Optional[EnvironmentConfig] = None
+    environment_config: Optional[EnvironmentConfig] = EnvironmentConfig()
 
 class DockerParams(BaseModel):
     docker_image: str
@@ -162,28 +162,20 @@ class AgentRunInput(BaseModel):
     inputs: Optional[Union[Dict, BaseModel, DockerParams]] = None
     agent_deployment: AgentDeployment
     orchestrator_runs: List['OrchestratorRun'] = []
-
-    def model_dict(self):
-        model_dict = self.dict()
-        for i, orchestrator_run in enumerate(model_dict['orchestrator_runs']):
-            for key, value in orchestrator_run.items():
-                if isinstance(value, datetime):
-                    model_dict['orchestrator_runs'][i][key] = value.isoformat()
-        return model_dict
-
+    
 class OrchestratorRunInput(BaseModel):
     consumer_id: str
-    inputs: Optional[Dict] = None
+    inputs: Optional[Union[Dict, BaseModel, DockerParams]] = None
     orchestrator_deployment: OrchestratorDeployment
     agent_deployments: List[AgentDeployment]
-    environment_deployment: Optional[EnvironmentDeployment] = None
+    environment_deployments: Optional[List[EnvironmentDeployment]] = None
 
 class OrchestratorRun(BaseModel):
     consumer_id: str
-    inputs: Optional[Dict] = None
+    inputs: Optional[Union[Dict, BaseModel, DockerParams]] = None
     orchestrator_deployment: OrchestratorDeployment
     agent_deployments: List[AgentDeployment]
-    environment_deployment: Optional[EnvironmentDeployment] = None
+    environment_deployments: Optional[List[EnvironmentDeployment]] = None
     status: str = "pending"
     error: bool = False
     id: Optional[str] = None
