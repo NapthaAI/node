586a60fb0661f937e774d964196e1a022c68d637
Richard Blythman <richardblythman@gmail.com>
2024-11-19 16:57:02 +0000

    add environment modules to hub

--- node/storage/hub/init_hub.py
+++ node/storage/hub/init_hub.py
@@ -27,6 +27,7 @@ def import_surql():
         f"{surql_path}/user.surql",
         f"{surql_path}/agent.surql",
         f"{surql_path}/orchestrator.surql",
+        f"{surql_path}/environment.surql",
         f"{surql_path}/persona.surql",
         f"{surql_path}/auth.surql",
         f"{surql_path}/node.surql",
