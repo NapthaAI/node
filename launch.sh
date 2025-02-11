13dfebc586a1ba3fb5ec37fbd0b7923cbae5890c
Enrico Rotundo <enrico.rotundo@gmail.com>
2025-02-11 17:35:22 +0100

    ci: print ollama latest vers

--- launch.sh
+++ launch.sh
@@ -179,8 +179,7 @@ EOF
     latest_version=$(curl -sf https://api.github.com/repos/ollama/ollama/releases/latest | 
                     grep '"tag_name":' | 
                     sed -E 's/.*"v([^"]+)".*/\1/')
-    echo "Latest version: $latest_version" | log_with_service_name "Ollama" $RED
-    
+
     if [ -z "$latest_version" ]; then
         echo "Failed to get latest version from GitHub" | log_with_service_name "Ollama" $RED
         exit 1
@@ -248,6 +247,7 @@ darwin_install_ollama() {
     latest_version=$(curl -sf https://api.github.com/repos/ollama/ollama/releases/latest | 
                     grep '"tag_name":' | 
                     sed -E 's/.*"v([^"]+)".*/\1/')
+    echo "Latest version: $latest_version" | log_with_service_name "Ollama" $RED
     
     if [ -z "$latest_version" ]; then
         echo "Failed to get latest version from GitHub" | log_with_service_name "Ollama" $RED
